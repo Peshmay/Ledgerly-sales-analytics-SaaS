86c0a16697805cfc08c777a227a5e392728d744e
Patience <peshmay@gmail.com>
2026-04-13 18:25:04 +0200

    update for client domain

--- ledgerly/client/src/App.tsx
+++ ledgerly/client/src/App.tsx
@@ -1,7 +1,15 @@
 import { AppRoutes } from "./routes/app-routes";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <AppRoutes />
+
+      <footer className="pt-3 text-center text-sm text-gray-500">
+        Built by Patience Evertsson
+      </footer>
+    </>
+  );
 }
 
 export default App;
