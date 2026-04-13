96e3b7c937418777cb370d256030ae602f3cff05
Patience <peshmay@gmail.com>
2026-04-13 21:24:29 +0200

    adding in health check

--- ledgerly/server/src/app.ts
+++ ledgerly/server/src/app.ts
@@ -7,7 +7,28 @@ import { errorHandler } from "./middleware/error.middleware";
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://ledgerly.peshifytech.com",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+  }),
+);
+
+
+
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
